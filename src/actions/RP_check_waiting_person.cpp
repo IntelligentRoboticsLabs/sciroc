2cd14220b6dd3ff9bb42e3545efd793e4f06ebfa
Francisco Martin Rico <fmrico@gmail.com>
2019-09-09 20:03:44 +0200

    Chech waiting zone
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

---
+++ src/actions/RP_check_waiting_person.cpp
@@ -0,0 +1,194 @@
+#include "RP_check_waiting_person.h"
+
+/* The implementation of RP_check_waiting_person.cpp */
+
+/* Esta acción se encarga de chequear si hay personas esperando en la entrada
+
+Estado inicial:
+- El robot está en wp_waiting, que está en la zona waiting
+- Hay una persona en wp_entry, que está en la zona entry
+
+Efecto de la acción
+- Persona detectada en la waiting zone
+*/
+
+#include <algorithm>
+#include <string>
+#include <list>
+#include <vector>
+
+/* constructor */
+RP_check_waiting_person::RP_check_waiting_person(const ros::NodeHandle& nh)
+: nh_(nh),
+  Action("check_waiting_person"),
+  wp_id_(),
+  obj_listener_(std::list<std::string>{"person"}, "base_footprint")
+{
+}
+
+void RP_check_waiting_person::activateCode()
+{
+  ROS_INFO("=============> Starting action");
+
+  for (size_t i = 0; i < last_msg_.parameters.size(); i++)
+  {
+    ROS_INFO("Param %s = %s", last_msg_.parameters[i].key.c_str(), last_msg_.parameters[i].value.c_str());
+    if (0 == last_msg_.parameters[i].key.compare("r"))
+      robot_id_ = last_msg_.parameters[i].value;
+    if (0 == last_msg_.parameters[i].key.compare("p"))
+      person_id_ = last_msg_.parameters[i].value;
+    if (0 == last_msg_.parameters[i].key.compare("wp"))
+      wp_id_ = last_msg_.parameters[i].value;
+    if (0 == last_msg_.parameters[i].key.compare("z"))
+      zone_id_ = last_msg_.parameters[i].value;
+  }
+
+  graph_.add_edge(robot_id_, "say: Checking if there are people waiting", robot_id_);
+  graph_.add_edge("leia", "want_see", zone_id_);
+
+  start_check_ = ros::Time::now();
+
+  obj_listener_.reset();
+  obj_listener_.set_working_frame(zone_id_);
+  obj_listener_.set_active();
+}
+
+void RP_check_waiting_person::deActivateCode()
+{
+  graph_.remove_edge("leia", "want_see", zone_id_);
+  obj_listener_.set_inactive();
+}
+
+
+void RP_check_waiting_person::step()
+{
+  if (!isActive())
+    return;
+
+  ROS_INFO("=============> Step RP_check_waiting_person");
+
+  if ((ros::Time::now() - start_check_).toSec() >= CHECK_WAITING_PERSON_CHECKING_TIME)
+  {
+    int num_people_waiting = 0;
+
+    obj_listener_.print();
+    obj_listener_.filter_objects("person", CHECK_WAITING_PERSON_MIN_PROBABILITY,
+      CHECK_WAITING_PERSON_OBJECT_MIN_X, CHECK_WAITING_PERSON_OBJECT_MAX_X, CHECK_WAITING_PERSON_OBJECT_MIN_Y,
+      CHECK_WAITING_PERSON_OBJECT_MAX_Z, CHECK_WAITING_PERSON_OBJECT_MIN_Z, CHECK_WAITING_PERSON_OBJECT_MAX_Z,
+      CHECK_WAITING_PERSON_OBJECT_MIN_SIZE_X, CHECK_WAITING_PERSON_OBJECT_MAX_SIZE_X, CHECK_WAITING_PERSON_OBJECT_MIN_SIZE_Y,
+      CHECK_WAITING_PERSON_OBJECT_MAX_SIZE_Y, CHECK_WAITING_PERSON_OBJECT_MIN_SIZE_Z, CHECK_WAITING_PERSON_OBJECT_MAX_SIZE_Z);
+
+    obj_listener_.print();
+    for (const auto& object : obj_listener_.get_objects())
+    {
+      if (object.class_id == "person")
+        num_people_waiting++;
+    }
+
+
+    // Crear nodo persona
+    // Arco hacia la persona 'waiting'
+
+    graph_.remove_edge("leia", "want_see", zone_id_);
+    obj_listener_.set_inactive();
+    if (num_people_waiting > 0)
+    {
+      graph_.add_edge(robot_id_, "say: I have found " + std::to_string(num_people_waiting) +
+        " people in the waiting zone.", robot_id_);
+      setSuccess();
+    }
+    else
+    {
+      graph_.add_edge(robot_id_, "say: There is nobody in the waiting zone.", robot_id_);
+      setFail();
+    }
+  }
+}
+
+void RP_check_waiting_person::qrCallback(const std_msgs::String::ConstPtr& qr)
+{
+  if (isActive())
+  {/*
+    std::list<bica_graph::StringEdge> edges_list =  graph_.get_string_edges();
+    for (auto it = edges_list.begin(); it != edges_list.end(); ++it)
+    {
+      std::string edge = it->get();
+      if (edge.find("has") != std::string::npos && it->get_source() == robot_id_)
+        graph_.remove_edge(*it);
+    }
+
+    YAML::Node root = YAML::Load(qr->data);
+    for (YAML::const_iterator it = root.begin(); it != root.end(); ++it)
+    {
+      graph_.add_node(it->as<std::string>(), "order");
+      graph_.add_edge(robot_id_, "has", it->as<std::string>());
+    }
+
+    edges_list =  graph_.get_string_edges();
+    for (auto it = edges_list.begin(); it != edges_list.end(); ++it)
+    {
+      std::string edge = it->get();
+      if (edge.find("wants") != std::string::npos)
+        order.push_back(it->get_target());
+      else if (edge.find("has") != std::string::npos)
+        order_in_robot.push_back(it->get_target());
+    }
+    std::sort(order.begin(), order.end());
+    std::sort(order_in_robot.begin(), order_in_robot.end());
+    if (order == order_in_robot)
+    {
+      for (auto it = edges_list.begin(); it != edges_list.end(); ++it)
+      {
+        std::string edge = it->get();
+        if (edge == "not needs" && graph_.get_node(it->get_target()).get_type() == "order")
+        {
+          graph_.remove_edge(*it);
+          graph_.remove_node(graph_.get_node(it->get_target()).get_id());
+        }
+        else if (edge == "needs" && graph_.get_node(it->get_target()).get_type() == "order")
+          graph_.remove_edge(*it);
+      }
+      graph_.add_edge(robot_id_, "say: Thank you! I will deliver the order.", "barman");
+    }
+    else
+    {
+      for (std::vector<std::string>::iterator it = order.begin(); it != order.end(); ++it)
+      {
+        if (std::find(order_in_robot.begin(), order_in_robot.end(), *it) == order_in_robot.end())
+          graph_.add_edge(robot_id_, "needs", *it);
+      }
+
+      for (std::vector<std::string>::iterator it = order_in_robot.begin(); it != order_in_robot.end(); ++it)
+      {
+        if (std::find(order.begin(), order.end(), *it) == order.end())
+          graph_.add_edge(robot_id_, "not needs", *it);
+      }
+      add_predicate("order_needs_fix leia");
+    }
+    setSuccess();
+    order.clear();
+    order_in_robot.clear();*/
+  }
+}
+
+/*-------------*/
+/* Main method */
+/*-------------*/
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "rosplan_interface_check_waiting_person");
+    ros::NodeHandle nh("~");
+
+    // create PDDL action subscriber
+    RP_check_waiting_person rpmb(nh);
+
+    // listen for action dispatch
+    ros::Subscriber ds = nh.subscribe("/kcl_rosplan/action_dispatch", 1000,
+      &KCL_rosplan::RPActionInterface::dispatchCallback,
+      dynamic_cast<KCL_rosplan::RPActionInterface*>(&rpmb));
+
+    rpmb.runActionInterface();
+
+    return 0;
+}
---
+++ src/restaurant/tests/restaurant_test_waiting_zone.cpp
@@ -0,0 +1,121 @@
+/*********************************************************************
+*  Software License Agreement (BSD License)
+*
+*   Copyright (c) 2018, Intelligent Robotics
+*   All rights reserved.
+*
+*   Redistribution and use in source and binary forms, with or without
+*   modification, are permitted provided that the following conditions
+*   are met:
+
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above
+*      copyright notice, this list of conditions and the following
+*      disclaimer in the documentation and/or other materials provided
+*      with the distribution.
+*    * Neither the name of Intelligent Robotics nor the names of its
+*      contributors may be used to endorse or promote products derived
+*      from this software without specific prior written permission.
+
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*   COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*   POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Jonatan Gines jonatan.gines@urjc.es */
+
+/* Mantainer: Jonatan Gines jonatan.gines@urjc.es */
+
+#include <ros/ros.h>
+
+#include <bica_planning/Executor.h>
+#include <bica/Component.h>
+#include <bica_graph/graph_client.h>
+
+class CheckWaitingZoneExecutor: public bica_planning::Executor, public bica::Component
+{
+public:
+  CheckWaitingZoneExecutor()
+  {
+    init_knowledge();
+    executed_ = false;
+  }
+
+  void init_knowledge()
+  {
+    add_instance("robot", "leia");
+    add_instance("person", "new_customer");
+    add_instance("zone", "waiting_zone");
+    add_predicate("robot_at leia wp_waiting_zone");
+    add_predicate("person_at new_customer wp_waiting_zone");
+    add_predicate("wp_in_zone wp_waiting_zone waiting_zone");
+    add_predicate("waypoint_at wp_waiting_zone main_room");
+
+
+    graph_.add_node("leia", "robot");
+    graph_.add_node("wp_waiting_zone", "waypoint");  // node is redundantelly added by graph-kms sync issue
+    graph_.add_node("waiting_zone", "zone");  // node is redundantelly added by graph-kms sync issue
+    graph_.add_node("main_room", "room");
+
+    graph_.set_tf_identity("main_room", "map");
+    graph_.set_tf_identity("base_footprint", "leia");
+
+    graph_.add_tf_edge("main_room", "leia");
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0);
+
+    tf2::Transform main2zone(q, tf2::Vector3(0.0, 0.0, 0.0));
+    graph_.add_edge("main_room", main2zone, "waiting_zone", true);
+  }
+
+  void step()
+  {
+    if (!executed_)
+    {
+      sleep(11);
+      ROS_INFO("Adding goal and planning");
+
+      add_goal("person_detected leia new_customer waiting_zone");
+      call_planner();
+      executed_ = true;
+    }
+    else
+      ROS_INFO("Finished executing CheckWaitingZoneExecutor");
+  }
+
+private:
+  ros::NodeHandle nh_;
+
+  bica_graph::GraphClient graph_;
+
+  bool executed_;
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "Restaurant");
+  ros::NodeHandle n;
+
+  ros::Rate loop_rate(1);
+  CheckWaitingZoneExecutor exec;
+  exec.setRoot();
+  exec.setActive(true);
+
+  while (exec.ok())
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
