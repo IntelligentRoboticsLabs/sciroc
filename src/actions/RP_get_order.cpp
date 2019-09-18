90fe56a3ff186d0add1d57e86d66c372feb5a63b
Lorena Bajo Rebollo <lbajo9@gmail.com>
2019-09-18 20:01:47 +0200

    order fixed

--- src/actions/RP_get_order.cpp
+++ src/actions/RP_get_order.cpp
@@ -98,6 +98,7 @@ void RP_get_order::step()
 
       obtained_order.id = "ORDER0";
       obtained_order.type = "Order";
+      obtained_order.table = table_id_;
       obtained_order.timestamp = gb_datahub::magicHour(boost::posix_time::to_iso_extended_string(ros::Time::now().toBoost()));
       obtained_order.status = "Pending";
 
